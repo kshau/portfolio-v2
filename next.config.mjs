89d4e0996762bd2a9633a6a7f1f08af79ad677f6
Shaurya Kumar <Shaurya>
2024-07-28 14:50:36 -0400

    alphabetically sorted redirects and added den redirect

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,18 @@ const nextConfig = {
     async redirects() {
         return [
             {
-                source: '/sat',
-                destination: 'https://discord.gg/enZFr8QQ33',
+                source: '/den',
+                destination: 'https://discord.gg/WqnSbKj37h',
                 permanent: true
             },
             {
                 source: '/playlist',
                 destination: 'https://open.spotify.com/playlist/3UqFRTl7cDqhifWgBoxuOA?si=4bd4bc420de240c5',
+                permanent: true
+            },
+            {
+                source: '/sat',
+                destination: 'https://discord.gg/enZFr8QQ33',
                 permanent: true
             },
         ]
