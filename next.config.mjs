53e07d4307b4d108a4e99a2a63460656bfd2551b
Shaurya Kumar <kshaurya731@gmail.com>
2025-10-08 22:44:06 -0400

    resume redirect

--- next.config.mjs
+++ next.config.mjs
@@ -3,18 +3,13 @@ const nextConfig = {
     async redirects() {
         return [
             {
-                source: '/den',
-                destination: 'https://discord.gg/WqnSbKj37h',
-                permanent: true
-            },
-            {
                 source: '/playlist',
                 destination: 'https://open.spotify.com/playlist/36NKwu4twh8xMCAOqvEiqV?si=a65ed062240148b8',
                 permanent: true
             },
             {
-                source: '/sat',
-                destination: 'https://discord.gg/enZFr8QQ33',
+                source: '/resume',
+                destination: 'https://drive.google.com/file/d/1deFsmyLI8x4x6OLnRmDscgpx1pGIq8NL/view?usp=sharing',
                 permanent: true
             },
         ]
